633a0c2ba74127b4e49e38511c3b5504341e88f4
Navodhya Yasisuru <73772623+Navodhya-2011@users.noreply.github.com>
2022-01-05 23:47:03 +0530

    OSSA test answers.

---
+++ Paper Number 4 2020.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+
+    int pid = fork();
+
+    if (pid == 0)
+    {
+        printf("%d\n", getppid());
+        sleep(50);  // create orphan process
+    }
+
+
+    return 0;
+}
---
+++ Paper Number 6 2020.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main()
+{
+
+    int pid = fork();
+
+    if(pid == 0){
+        for (int i = 1; i < 100; i+=2)
+        {
+            printf("%d\n", i);
+        }
+
+    }
+
+
+    return 0;
+}
